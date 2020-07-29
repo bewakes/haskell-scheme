e33883630d1a4444a275b09440d396d7cf07ec9e
bewakes <bewakepandey@gmail.com>
2020-07-29 18:10:07 +0545

    Re-Initialize project

--- README.md
+++ README.md
@@ -1,16 +1,5 @@
-# scheme
-Implementation of Scheme in haskell. This is basically exercies from [Write yourself a Scheme in 48 hours](https://en.wikibooks.org/wiki/Write_Yourself_a_Scheme_in_48_Hours)
+# haskell-scheme
+Scheme interpretor. Learning More of Haskell and some of Scheme.
 
-## Features
-* [x] Basic Data Types(number, string, character)
-* [x] Functions
-* [x] Lambdas
-
-Basically general non-IO related programs can be written. For example: calculate factorial.
-
-## TODO
-* [ ] Support multiline code and running a file
-* [ ] Interpretor history
-* [ ] Basic IO
-* [ ] Standard Library functions
-* [ ] Add types
+This is the implementation from [Write you a Scheme(Newer)](https://www.wespiser.com/writings/wyas/home.html)
+Previous implementation from [Write yourself a Scheme in 48 hours](https://en.wikibooks.org/wiki/Write_Yourself_a_Scheme_in_48_Hours) can be found in `old-interpretor` branch.
--- app/Main.hs
+++ app/Main.hs
@@ -1,48 +1,6 @@
 module Main where
 
-import           Control.Monad
-import           Control.Monad.Except
-import           System.IO
-
-import           Evaluator
-import           Lisp
-import           Parser
-
-evalString :: Env -> String -> IO String
-evalString env x = runIOThrows $ show <$> (liftThrows (readExpr x) >>= eval env)
-
-evalAndPrint :: Env -> String -> IO ()
-evalAndPrint env expr = evalString env expr >>= putStrLn
-
--- print a string and immediately flush the stream, otherwise output might sit in output buffers
--- and user will never see prompts or results
-flushStr :: String -> IO ()
-flushStr str = putStr str >> hFlush stdout
-
-readPrompt :: String -> IO String
-readPrompt prompt = flushStr prompt >> getLine
-
-until_ :: Monad m => (a -> Bool) -> m a -> (a -> m()) -> m ()
-until_ pred prompt action = do
-    result <- prompt
-    if pred result
-       then return ()
-       else action result >> until_ pred prompt action
-
-runOne :: String -> IO ()
-runOne expr = primitiveBindings >>= (`evalAndPrint` expr)
-
-
-runRepl :: IO ()
-runRepl = primitiveBindings >>= until_ (== "quit") (readPrompt "> ") . evalAndPrint
-
-runFile :: IO ()
-runFile = do
-  fileContent <- readFile "programs/program.sch"
-  env <- nullEnv
-  let statements = lines fileContent
-  lst <- mapM (evalString env) statements
-  mapM_ print $ zipWith (\x y -> x ++ " ---> " ++ y) statements lst
+import Lib
 
 main :: IO ()
-main = runRepl
+main = someFunc
